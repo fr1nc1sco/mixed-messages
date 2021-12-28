74cd672c238ee46ef4c0f7e68e5d7b1e051496a8
F.J.Revoreda <francisco09j23r@gmail.com>
2021-12-27 20:04:12 -0800

    Removed array string func, implemented mArray

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,18 +1,14 @@
-const gOne = ["Hello friend! ", "Hola amigo! ", "Aloha hoa! ", "Konnichiwa tomodachi! ", "Bonjour mon ami! "];
-const gTwo = ["How are you? ", "Como estas? ", "Howzit? ", "Genkideska? ", "Ca va? "];
-const gThree = ["I hope you are well. ", "Espero que estes bien. ", "Maika'i mana'olana au ia 'oe. ", "Anata ga genkidearu koto o negatte imasu. ", "J’espère que vous allez bien. "];
-const gFour = ["Bye!", "Adios!", "Aloha!", "Sayonara!", "Au revoir!"];
-
-
-function randomArrayString(array) {
-    return array[Math.floor(Math.random()*array.length)];
-}
+const mOne = ["Hello friend! ", "Hola amigo! ", "Aloha hoa! ", "Konnichiwa tomodachi! ", "Bonjour mon ami! "];
+const mTwo = ["How are you? ", "Como estas? ", "Howzit? ", "Genkideska? ", "Ca va? "];
+const mThree = ["I hope you are well. ", "Espero que estes bien. ", "Maika'i mana'olana au ia 'oe. ", "Anata ga genkidearu koto o negatte imasu. ", "J’espère que vous allez bien. "];
+const mFour = ["Goodbye!", "Adios!", "Aloha!", "Sayonara!", "Au revoir!"];
+const mArray = [mOne, mTwo, mThree, mFour];
 
 function messageGenerator() {
-    let message = randomArrayString(gOne);
-    message += randomArrayString(gTwo);
-    message += randomArrayString(gThree);
-    message += randomArrayString(gFour);
+    let message = '';
+    mArray.forEach(array => {
+        message += array[Math.floor(Math.random()*array.length)];
+    })
     return message;
 }
 
