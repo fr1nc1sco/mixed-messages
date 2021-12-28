610f36e9c8c594ebbf77c99880e0cc6217c68719
F.J.Revoreda <francisco09j23r@gmail.com>
2021-12-27 17:00:30 -0800

    Updated js file

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,6 +1,3 @@
 const messages = ['hello', 'goodbye', 'aloha', 'hola', 'konnichiwa'];
-let randIndex = 0;
-for (let i = 0; i < messages.length; i++) {
-    randIndex = Math.floor(Math.random()*messages.length);
-    console.log(messages[randIndex]);
-}
+let randIndex = Math.floor(Math.random()*messages.length);
+console.log(messages[randIndex]);
